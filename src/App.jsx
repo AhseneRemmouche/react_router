531544230bd30fb47eb7dac557a8638ee7a4aacc
AhseneRemmouche <ahsene.remmouche@outlook.com>
2024-11-25 21:22:09 -0500

    add new routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,34 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
 } from "react-router-dom";
 import RouteLayout from "./layout/RouteLayout";
 import ContactInfo from "./components/ContactInfo";
 import ContactForm from "./components/ContactForm";
 import ContactLayout from "./layout/ContactLayout";
+import NotFound from "./components/NotFound";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RouteLayout />}>
-        <Route index element={<Home />} />
-        <Route path="products" element={<Products />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<ContactLayout />}>
-          <Route path="info" element={<ContactInfo />} />
-          <Route path="form" element={<ContactForm />} />
-        </Route>
-      </Route>
-    )
-  );
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<RouteLayout />}>
+				<Route index element={<Home />} />
+				<Route path="products" element={<Products />} />
+				<Route path="about" element={<About />} />
+				<Route path="contact" element={<ContactLayout />}>
+					<Route path="info" element={<ContactInfo />} />
+					<Route path="form" element={<ContactForm />} />
+				</Route>
+				<Route path="*" element={<NotFound />} />
+			</Route>
+		)
+	);
 
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Contact() {
-	const navigate = useNavigate()
-  return (
-    <div>
-      <h1>Contact Page</h1>
-      <div className="contact_buttons">
-        <button onClick={() => navigate("/contact/info")}>Info</button>
-        <button onClick={() => navigate("/contact/form")}>Form</button>
-      </div>
-    </div>
-  );
+	const navigate = useNavigate();
+	return (
+		<div>
+			<h1>Contact Page</h1>
+			<div className="contact_buttons">
+				<button onClick={() => navigate("info")}>Info</button>
+				<button onClick={() => navigate("form")}>Form</button>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
